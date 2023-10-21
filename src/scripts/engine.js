914c621bcd1cb18648832e7dd97946f974c255b4
MarcosWinther <winthermarcos@gmail.com>
2023-10-21 20:20:56 -0300

    Implementado a lógica de duelos

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -97,8 +97,32 @@ async function setCardsField(cardId) {
 
    let duelResults = await checkDuelResults(cardId, computerCardId);
 
-   // await updateScore();
-   // await drawButton(duelResults);
+   await updateScore();
+   await drawButton(duelResults);
+}
+
+async function drawButton(text) {
+   state.actions.button.innerText = text;
+   state.actions.button.style.display = 'block';
+}
+
+async function updateScore() {
+   state.score.scoreBox.innerText = `Win: ${state.score.playerScore} | Lose: ${state.score.computerScore}`;
+}
+
+async function checkDuelResults(playerCardId, computerCardId) {
+   let duelResults = 'Empate';
+   let playerCard = cardData[playerCardId];
+
+   if (playerCard.WinOf.includes(computerCardId)) {
+      duelResults = 'Ganhou';
+      state.score.playerScore++;
+   } else if (playerCard.WinOf.includes(computerCardId)) {
+      duelResults = 'Perdeu';
+      state.score.computerScore++;
+   }
+
+   return duelResults;
 }
 
 async function removeAllCardsImages() {
